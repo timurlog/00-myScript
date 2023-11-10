7c53aa6fa7adb4b1f4adf4b9acd02c5cdffa6c9e
timurlog <timur.logie@gmail.com>
2023-11-10 18:22:39 +0100

    modified autoexec.sh

--- autoexec.sh
+++ autoexec.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-mkdir -p public/{css/,js/,src/,fonts/,pages/} && touch index.html public/css/style.css public/js/main.js
+mkdir -p public/{css/,js/,src/,fonts/,pages/}
+touch index.html 
+touch public/css/style.css 
+touch public/js/main.js
+
 
 sleep 1
 
 tree
 
-echo "autoexec executer"
+echo "###################################"
+echo "#                                 #"
+echo "#      AUTOEXEC EST EXECUTE       #"
+echo "#                                 #"
+echo "###################################"
