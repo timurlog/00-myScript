58cdabd6ac24cef44e60873b4bfd96d1ad0a29b5
timurlog <timur.logie@gmail.com>
2023-11-21 11:22:23 +0100

    modified autoexec.sh

--- autoexec.sh
+++ autoexec.sh
@@ -15,4 +15,69 @@ echo "#                                 #"
 echo "###################################"
 echo
 
+# Redirection du contenu HTML dans index.html
+cat <<HTML_EOF > index.html
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <!-- LINKS -->
+    <link rel="stylesheet" href="./public/style/style.css">
+    <link rel="shortcut icon" href="" type="image/x-icon">
+</head>
+<body>
+    <!-- HEADER -->
+
+
+
+    <!-- MAIN -->
+
+
+
+    <!-- FOOTER -->
+
+
+
+    <!-- SCRIPTS -->
+    <script src="./public/js/main.js"></script>
+</body>
+</html>
+HTML_EOF
+
+# Redirection du contenu CSS dans style.css
+cat <<CSS_EOF > public/style/style.css
+/* FONTS */
+
+
+
+/* BODY */
+
+* {
+    margin: 0;
+    padding: 0;
+}
+
+body {
+    background-color: #FFFFFF;
+    font-family: Arial, Helvetica, sans-serif;
+}
+
+/* HEADER */
+
+
+
+/* MAIN */
+
+
+
+/* FOOTER */
+
+
+
+/* RESPONSIVE */
+CSS_EOF
+
 tree
