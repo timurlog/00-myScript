57e0fc967ad6d79efe7561cea5f066069260adfa
timurlog <timur.logie@gmail.com>
2023-11-27 16:29:48 +0100

    modified

--- autoexec.sh
+++ autoexec.sh
@@ -59,6 +59,56 @@ cat <<HTML_EOF > index.html
 </html>
 HTML_EOF
 
+# style.css
+cat <<CSS_EOF > public/style/style.css
+/* ########### FONTS ############ */
+
+
+/* ############ BODY ############  */
+* {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    text-decoration: none;
+    list-style: none;
+}
+
+body {
+    background-color: #FFFFFF;
+    font-family: Arial, Helvetica, sans-serif;
+}
+
+/* ########### HEADER ########### */
+
+
+/* ############ MAIN ############ */
+
+
+/* ########### FOOTER ########### */
+
+
+/* ######### RESPONSIVE ######### */
+/* ### SMARTPHONE ### */
+@media (max-width: 576px) {
+
+}
+
+/* ### TABLET ### */
+@media (min-width: 577px) and (max-width: 768px) {
+
+}
+
+/* ### DESKTOP ### */
+@media (min-width: 769px) and (max-width: 992px) {
+
+}
+
+/* ### BIG DESKTOP ### */
+@media (min-width: 993px) {
+
+}
+CSS_EOF
+
 # style.sass
 cat <<CSS_EOF > src/sass/style.sass
 /* ########### FONTS ############ */
