47fc24de9eea25321d7390e770d382d6b605a3af
timurlog <timur.logie@gmail.com>
2023-11-27 14:03:42 +0100

    modified autoexec.sh added sass

--- autoexec.sh
+++ autoexec.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-mkdir -p public/{style/,js/,src/{image/,fonts/,video/,music/},page/}
+mkdir -p public/{style/,js/,src/{image/,fonts/,video/,music/,sass/},page/}
 touch index.html 
-touch public/style/style.css 
+touch public/style/style.css
+touch public/src/sass/style.sass
 touch public/js/main.js
 
 sleep 1
@@ -24,84 +25,80 @@ cat <<HTML_EOF > index.html
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 
-    <!-- LINKS -->
+    <!-- ########## LINKS ########### -->
     <link rel="stylesheet" href="./public/style/style.css">
-    <link rel="shortcut icon" href="" type="image/x-icon">
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <link rel="shortcut icon" href="" type="image/x-icon">
+
 </head>
 <body>
-    <!-- HEADER -->
+    <!-- ########## HEADER ########## -->
+    <header class='header'>
 
+    </header>
 
+    <!-- ########### MAIN ########### -->
+    <main>
+        <!-- ###  ### -->
+        <section>
 
-    <!-- MAIN -->
+        </section>
+    </main>
 
+    <!-- ########## FOOTER ########## -->
+    <footer class='footer'>
 
+    </footer>
 
-    <!-- FOOTER -->
-
-
-
-    <!-- SCRIPTS -->
+    <!-- ########## SCRIPTS ######### -->
     <script src="./public/js/main.js"></script>
+
 </body>
 </html>
 HTML_EOF
 
-# style.css
-cat <<CSS_EOF > public/style/style.css
-/* FONTS */
+# style.sass
+cat <<CSS_EOF > public/src/sass/style.sass
+/* ########### FONTS ############ */
 
 
+/* ############ BODY ############  */
+*
+    box-sizing: border-box
+    margin: 0
+    padding: 0
+    text-decoration: none
+    list-style: none
 
-/* BODY */
+body
+    background-color: #FFFFFF
+    font-family: Arial, Helvetica, sans-serif
 
-* {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-    text-decoration: none;
-    list-style: none;
-}
-
-body {
-    background-color: #FFFFFF;
-    font-family: Arial, Helvetica, sans-serif;
-}
-
-/* HEADER */
+/* ########### HEADER ########### */
 
 
-
-/* MAIN */
-
+/* ############ MAIN ############ */
 
 
-/* FOOTER */
+/* ########### FOOTER ########### */
 
 
+/* ######### RESPONSIVE ######### */
+/* ### SMARTPHONE ### */
+@media (max-width: 576px)
 
-/* RESPONSIVE */
 
-/* SMARTPHONE */
-@media (max-width: 576px) {
+/* ### TABLET ### */
+@media (min-width: 577px) and (max-width: 768px)
 
-}
 
-/* TABLET */
-@media (min-width: 577px) and (max-width: 768px) {
-    
-}
+/* ### DESKTOP ### */
+@media (min-width: 769px) and (max-width: 992px)
 
-/* DESKTOP */
-@media (min-width: 769px) and (max-width: 992px) {
-    
-}
 
-/* BIG DESKTOP */
-@media (min-width: 993px) {
+/* ### BIG DESKTOP ### */
+@media (min-width: 993px)
 
-}
 CSS_EOF
 
 tree
