3cba87a7d56d23173e8982a900eaed309732f983
timurlog <timur.logie@gmail.com>
2023-11-28 09:57:15 +0100

    modified autoexec

--- autoexec.sh
+++ autoexec.sh
@@ -2,7 +2,7 @@
 
 mkdir -p public/{style/,js/,page/,assets/{image/,fonts/,video/,music/}}
 mkdir -p src/sass/modules
-touch index.html public/style/style.css public/js/main.js src/sass/app.sass src/sass/_variables.sass src/sass/_fonts.sass src/sass/modules/_body.sass src/sass/modules/_header.sass src/sass/modules/_footer.sass src/sass/modules/_main.sass src/sass/modules/_section1.sass
+touch index.html public/style/style.css public/js/main.js src/sass/app.sass src/sass/_variables.sass src/sass/_fonts.sass src/sass/modules/_body.sass src/sass/modules/_header.sass src/sass/modules/_footer.sass src/sass/modules/_main.sass src/sass/modules/_section-1.sass
 
 sleep 1
 
@@ -197,8 +197,8 @@ cat <<SASS_EOF > src/sass/modules/_main.sass
 
 SASS_EOF
 
-# _section1.sass
-cat <<SASS_EOF > src/sass/modules/_section1.sass
+# _section-1.sass
+cat <<SASS_EOF > src/sass/modules/_section-1.sass
 /* ### SECTION 1 ### */
 
 
